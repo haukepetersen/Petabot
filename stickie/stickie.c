37706cc02f14b5abd686f7d9d448564fcf89f139
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-05-31 16:52:18 +0200

    stickie: ready for action

--- stickie/stickie.c
+++ stickie/stickie.c
@@ -9,16 +9,29 @@
 #include <fcntl.h>
 #include <err.h>
 #include <linux/serial.h>
+#include <pthread.h>
 
-#define TYPE_BUTTON         1
-#define TYPE_AXIS           2
+#define TYPE_BUTTON         (1U)
+#define TYPE_AXIS           (2U)
 
 #define BAUDRATE            (B500000)
+
+#define UPDATE_INTERVAL     (20 * 1000)     /* 20ms */
+
+#define AXIS_STEERING       (3)
+#define AXIS_FW             (2)
+#define AXIS_BW             (5)
+
+static int remote;
+static int js;
+
+static volatile int dir = 0;
+static volatile int speed = 0;
 
 static int buttons = 0;
 static int axes = 0;
 
-static int open_remote(const char *dev)
+static int open_remote(const char *dev, int baudrate)
 {
     int fd;
     struct termios options;
@@ -30,8 +43,8 @@ static int open_remote(const char *dev)
 
     fcntl(fd, F_SETFL, 0);
     tcgetattr(fd, &options);
-    cfsetispeed(&options, BAUDRATE);
-    cfsetospeed(&options, BAUDRATE);
+    cfsetispeed(&options, baudrate);
+    cfsetospeed(&options, baudrate);
     cfmakeraw(&options);
     options.c_cflag |= (CLOCAL | CREAD);
     options.c_cflag &= ~CRTSCTS;
@@ -39,20 +52,21 @@ static int open_remote(const char *dev)
         return -1;
     }
 
+    printf("Found Remote: %s @ %ibps\n", dev, 500000);
     return fd;
 }
 
 static int open_stick(const char *dev)
 {
-    int js;
+    int fd;
     uint8_t buf[8];
 
-    js = open(dev, O_RDONLY);
-    if (js < 0) {
-        return js;
+    fd = open(dev, O_RDONLY);
+    if (fd < 0) {
+        return fd;
     }
 
-    read(js, buf, 8);
+    read(fd, buf, 8);
     while (buf[6] & 0x80) {
         if (buf[6] == 0x81) {
             ++buttons;
@@ -60,64 +74,29 @@ static int open_stick(const char *dev)
         if (buf[6] == 0x82) {
             ++axes;
         }
-        read(js, buf, 8);
+        read(fd, buf, 8);
     };
 
     printf("Found Joystick: %i buttons, %i axes\n", buttons, axes);
-    return js;
+    return fd;
 }
 
-int main(int argc, char **argv)
+static void *stick_reader(void *arg)
 {
-    int js;
-    int remote;
-
     ssize_t n;
     uint8_t buf[8];
 
-    char line[128];
-    int line_len;
+    int newdata = 0;
 
-    uint8_t newdat = 0;
+    int16_t steering = 0;
+    int16_t fw = -32767;
+    int16_t bw = -32767;
 
-    int axis_x;
-    int axis_y;
-
-    int16_t x;
-    int16_t y;
-
-    if (argc < 5) {
-        printf("usage: %s <device> <x-axis> <y-axis> <antenna dev>\n", argv[0]);
-        return 1;
-    }
-    axis_x = atoi(argv[2]);
-    axis_y = atoi(argv[3]);
-
-    js = open_stick(argv[1]);
-    if (js < 0) {
-        puts("error: unable to open joystick");
-        return 1;
-    }
-
-    /* set baudrate */
-    remote = open_remote(argv[4]);
-    if (remote < 0) {
-        puts("error: unable to open output port");
-        return 1;
-    }
+    printf("\n     steering  speed_fw  speed_bw\n");
 
     while (1) {
         n = read(js, buf, 8);
         if (n == 8) {
-
-            if (buf[6] & 0x80) {
-                printf("config ");
-                for (int i = 0; i < 8; i++) {
-                    printf("0x%02x ", buf[i]);
-                }
-                printf("\n");
-                continue;
-            }
 
             // if (buf[6] == TYPE_BUTTON) {
             //     if (buf[4]) {
@@ -126,45 +105,87 @@ int main(int argc, char **argv)
             //         printf("button %u released\n", buf[7]);
             //     }
             // }
-            else if (buf[6] == TYPE_AXIS) {
-                if (buf[7] == axis_x) {
-                    x = ((int16_t)buf[5] << 8) | buf[4];
-                    newdat = 1;
-                } else if (buf[7] == axis_y) {
-                    y = ((int16_t)buf[5] << 8) | buf[4];
-                    newdat = 1;
+
+            if (buf[6] == TYPE_AXIS) {
+                if (buf[7] == AXIS_STEERING) {
+                    steering = ((int16_t)buf[5] << 8) | buf[4];
+                    newdata = 1;
+                }
+                else if (buf[7] == AXIS_FW) {
+                    fw = ((int16_t)buf[5] << 8) | buf[4];
+                    newdata = 1;
+                }
+                else if (buf[7] == AXIS_BW) {
+                    bw = ((int16_t)buf[5] << 8) | buf[4];
+                    newdata = 1;
                 }
             }
 
-            if (newdat) {
-                newdat = 0;
-                printf("raw: %7i %7i\n", x, y);
-                x = (-1) * x / 32;
-                y = (-1) * y / 32;
-                if (x > -200 && x < 200) {
-                    x = 0;
+            if (newdata) {
+                newdata = 0;
+                int a = fw + 32767;
+                int b = bw + 32767;
+
+                /* scale values to actual control values */
+                if (a == 0) {
+                    speed = -(b / 64);
                 }
-                // if (y > -200 && y < 200) {
-                //     y = 0;
-                // }
-                printf("ctrl %7i %7i\n", x, y);
+                else {
+                    speed = a / 64;
+                }
+                dir = -1 * (steering / 32);
 
-                printf("\033[F\033[F");
-
-
-                line_len = sprintf(line, "ctrl %i %i\n", x, y);
-                write(remote, line, line_len);
-                // line_len = read(remote, line, sizeof(line));
-                // line[line_len] = '\0';
-                // printf("res: %s\n", line);
+                printf("raw:  %7i   %7u   %7u\n", steering, a, b);
+                puts("");
+                printf("        speed  steering\n");
+                printf("ctrl  %7i   %7i\n", speed, dir);
+                printf("\033[F\033[F\033[F\033[F");
             }
-        }
-        else {
-            puts("input not length 8");
         }
     }
 
+    return NULL;
+}
 
+int main(int argc, char **argv)
+{
+    pthread_t reader_thread;
+    int res;
+
+    char line[128];
+    int line_len;
+
+    if (argc < 3) {
+        printf("usage: %s <device> <antenna dev>\n", argv[0]);
+        return 1;
+    }
+
+    js = open_stick(argv[1]);
+    if (js < 0) {
+        puts("error: unable to open joystick");
+        return 1;
+    }
+
+    remote = open_remote(argv[2], BAUDRATE);
+    if (remote < 0) {
+        puts("error: unable to open output port");
+        return 1;
+    }
+
+    /* start the reader thread */
+    res = pthread_create(&reader_thread, NULL, stick_reader, NULL);
+    if (res < 0) {
+        puts("error: unable to start reader thread");
+        return 1;
+    }
+
+    while (1) {
+        usleep(UPDATE_INTERVAL);
+        line_len = sprintf(line, "ctrl %i %i\n", speed, dir);
+        write(remote, line, line_len);
+    }
+
+    pthread_join(reader_thread, NULL);
     close(js);
     return 0;
 }
